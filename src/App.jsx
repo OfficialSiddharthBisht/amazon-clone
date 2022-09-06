4296a97d8498d2698a258834ffe1defcd5b04c44
Siddharth <officialsiddharthbisht@gmail.com>
2022-09-06 14:04:00 +0530

    ADD: Basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,16 @@ import {Routes,Route} from 'react-router-dom';
 
 // components import
 import Home from './components/Home'
+import Checkout from './components/Checkout';
+import Login from './components/Login';
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div className="app">
+      <Routes>
+        <Route path='/checkout' element = {<Checkout/>} />
+        <Route path='/login' element = {<Login/>} />
+        <Route path='/' element = {<Home/>} />
+      </Routes>
     </div>
   );
 }
